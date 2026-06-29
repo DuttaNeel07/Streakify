1ca7138622368e9ba0ad5a6c538168dbd75c97fe
Neel Dutta <neel.dutta0729@gmail.com>
2026-06-29 15:53:36 +0530

    add: function to scan the directory and save path to root folder

--- scan.go
+++ scan.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
-	"strings"
 	"os"
+	"os/user"
+	"strings"
 )
 
 func scan(folder string){
@@ -53,3 +57,81 @@ func recursiveScanFolder(folder string) []string{
 	return scanGitFolders(make ([]string, 0), folder)
 }
 
+//creating a function to return the location of dot file to get repo lists, if not present it creates the location for the user
+
+func getDotFilePath() string{
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+
+	return dotFile
+}
+
+func addNewSliceElementsToFile(filepath string, newRepos []string){
+	existingRepos:= parseFileLinestoSlice(filepath)
+	repos := joinSlices(newRepos, existingRepos)
+}
+
+func parseFileLinestoSlice(filepath string) []string{
+	f := os.OpenFile(filepath)
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil{
+		if err != io.EOF{
+			panic(err)
+		}
+	}
+
+	return lines
+}
+//This function opens file located at filepath
+func openFile(filepath string) *os.File{
+	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY, 0755)
+
+	if err != nil{
+		if os.IsNotExist(err){
+			_, err := os.Create(filepath)
+			if err != nil{
+				panic(err)
+			}
+		} else{
+			panic(err)
+		}
+	}
+
+	return f
+}
+
+func joinSlices(new []string, existing []string) []string{
+	for _,i := range new{
+		if !sliceContains(existing, i){
+			existing = append(existing, i)
+		}
+	}
+return existing
+}
+
+func sliceContains(slice []string, value string) bool{
+	for _,v := range slice{
+		if v == value{
+			return true
+		}
+	}
+	return false
+}
+
+//This function takes the path and the slice and writes the slice on the file with each line onto a new line
+
+func dumpStringsSlicetoFile(repos []string, filepath string){
+	content := strings.Join(repos, "\n")
+	os.WriteFile(filepath, []byte(content), 0755)
+} 
